fe74b0bcc3c57ad141dcbdc2f8b9e9d32bb3922f
takumon <inouetakumon@gmail.com>
2018-10-13 21:43:49 +0900

    ソースコードにタイトルを設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-code-titles`,
           {
             resolve: `gatsby-remark-images`,
             options: {
