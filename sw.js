fb43157950d86c548fa3da90c777e539bc705120
Takumon <inouetakumon@gmail.com>
2018-12-29 16:03:47 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a3d396acbe3484503ba1.js"
+    "url": "webpack-runtime-bacba87bf1d6ec28e892.js"
   },
   {
     "url": "app.319712d3e7fffa8163a7.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cab32fcba75ea6d7ce93921cec3c27b2"
+    "revision": "0aca6afb0e7ae984c16540493ff885d8"
   },
   {
     "url": "component---src-pages-404-js-1883e4ce94d1c665f254.js"
