bfd35274a5111d46f715371c7d5e318130615c36
Takumon <inouetakumon@gmail.com>
2019-01-05 21:49:29 +0900

    #63 URLにプロトコルを明示

--- src/components/iframely/index.js
+++ src/components/iframely/index.js
@@ -6,14 +6,16 @@ export default class Iframely extends React.Component {
 
   // 記事ページから古い記事に遷移したときにもロードされるようにする
   componentDidMount () {
-    window.iframely.load();
+    if (window.iframely) {
+      window.iframely.load();
+    }
   }
 
 
   render() {
       return (
         <Helmet>
-          <script type="text/javascript" src="//cdn.iframe.ly/embed.js" charset="utf-8"/>
+          <script type="text/javascript" src="https://cdn.iframe.ly/embed.js" charset="utf-8"/>
         </Helmet>
     );
   }
