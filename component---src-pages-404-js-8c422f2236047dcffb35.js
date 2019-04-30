554854792ba4309622ba0ec14faf2fad3bf148a0
Takumon <inouetakumon@gmail.com>
2019-04-30 21:18:01 +0900

    Updates

---
+++ component---src-pages-404-js-8c422f2236047dcffb35.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{154:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
