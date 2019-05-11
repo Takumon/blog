cdba87c983ec49a53c9ee31362a560a19712fb9e
Takumon <takumontakumontakumon@yahoo.co.jp>
2019-05-11 14:32:11 +0900

    #103 一覧にサムネイル画像を表示 (#121)
    fix #103

--- src/components/post-preview/index.js
+++ src/components/post-preview/index.js
@@ -3,25 +3,38 @@ import { Link } from 'gatsby'
 
 import PostMetaInfo from '../post-meta-info';
 import styles from './index.module.scss';
+import * as config from '../../config/blog-config.js';
 
 
 
 class PostPreview extends React.Component {
   render() {
-    const {slug, title, excerpt, date, tags} = this.props.postField;
+    const {
+      slug,
+      title,
+      excerpt,
+      date,
+      tags,
+      thumbnail,
+    } = this.props.postField;
 
     return (
       <article key={slug} className={styles.content}>
-        <h3 className={styles.title} >
-          <Link className={styles.title_link} to={slug}>
-            {title}
-          </Link>
-        </h3>
-        <p dangerouslySetInnerHTML={{ __html: excerpt }} />
-        <PostMetaInfo
-          tags={tags}
-          date={date}
-          />
+        <div className={styles.content_thumbnail}>
+          <img className={styles.content_thumbnail_image} src={thumbnail || config.blogImageUrl} />
+        </div>
+        <div className={styles.content_post_info}>
+          <h3 className={styles.title} >
+            <Link className={styles.title_link} to={slug}>
+              {title}
+            </Link>
+          </h3>
+          <p className={styles.content_text} dangerouslySetInnerHTML={{ __html: excerpt }} />
+          <PostMetaInfo
+            tags={tags}
+            date={date}
+            />
+        </div>
       </article>
     );
   }
