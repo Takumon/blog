587d5af2423215e20fae3e25d567a0d777af41a2
Takumon <inouetakumon@gmail.com>
2019-05-13 18:16:07 +0900

    #127 フォーマット修正

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 
@@ -27,12 +27,15 @@ export default ({
     `}
 
     render={(data) => {
-      const image = data.images.edges.find(n => {
-        return n.node.relativePath.includes(filename);
-      });
-      if (!image) { return null; }
-      
-      const imageSizes = image.node.childImageSharp.sizes;
+      const image = data.images.edges.find(n =>
+        n.node.relativePath.includes(filename)
+      )
+
+      if (!image) {
+        return null
+      }
+
+      const imageSizes = image.node.childImageSharp.sizes
       return (
         <Img
           alt={alt}
