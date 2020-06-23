609c9a841c25a9b48eb88eea82a2f81e5c8fad92
Takumon <inouetakumon@gmail.com>
2020-06-23 09:13:41 +0900

    #43 add pagination (#183)

--- src/components/post-list/index.js
+++ src/components/post-list/index.js
@@ -4,17 +4,50 @@ import PostPreview from '../post-preview';
 
 import styles from './index.module.scss';
 
+const PER_PAGE = 15;
+
 class PostList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+    };
+    this.loadNextPage = this.loadNextPage.bind(this);
+  }
+
+  hasNextPage() {
+    return this.state.page * PER_PAGE < this.props.postFields.length;
+  }
+
+  loadNextPage() {
+    if (!this.hasNextPage()) {
+      return;
+    }
+
+    this.setState({
+      page: this.state.page + 1,
+    })
+  }
 
   render() {
+    const { postFields } = this.props;
+    const filtered = postFields.slice(0, this.state.page * PER_PAGE);
+
     return (
       <div className={styles.content}>
-        {this.props.postFields.map(postField =>
-          <PostPreview
-            key={postField.slug}
-            postField={postField}
-            />
-        )}
+        <div className={styles.content_inner} >
+          {filtered.map(postField =>
+            <PostPreview
+              key={postField.slug}
+              postField={postField}
+              />
+          )}
+        </div>
+        {this.hasNextPage() ? (
+          <div className={styles.pagination_area}>
+            <button className={styles.pagination_button} onClick={this.loadNextPage}>Load More</button>
+          </div>
+        ) : null}
       </div>
     );
   }
