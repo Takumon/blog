24e8458ae14d933d5f2363e2d408a2e961a73adc
Takumon <inouetakumon@gmail.com>
2020-06-23 15:54:41 +0900

    fix

--- src/components/background-image/index.js
+++ src/components/background-image/index.js
@@ -7,14 +7,14 @@ export default function useBackgroundImages() {
     query {
       headerImage: file(relativePath: {eq: "background.jpg"}) {
         childImageSharp {
-          fluid(maxWidth: 1500, quality: 100, pngQuality: 100) {
+          fluid(maxWidth: 1500, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
       footerImage: file(relativePath: {eq: "background-for-footer.jpg"}) {
         childImageSharp {
-          fluid(maxWidth: 1500, quality: 100, pngQuality: 100) {
+          fluid(maxWidth: 1500, quality: 100) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
