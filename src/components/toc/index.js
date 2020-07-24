1b2e49b53905b55fe797c7febde8603bd27e1a13
Takumon <inouetakumon@gmail.com>
2020-07-24 17:06:58 +0900

    #195 incorporate use debounce

--- src/components/toc/index.js
+++ src/components/toc/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Location } from '@reach/router';
+import { Location } from '@reach/router'
 import { Link } from 'gatsby'
 
-
-import styles from './index.module.scss';
+import styles from './index.module.scss'
 
 class Toc extends React.Component {
   render() {
@@ -11,24 +10,30 @@ class Toc extends React.Component {
 
     return (
       <Location>
-        {({ navigate, location }) =>
-          <div className={styles.content} >
+        {({ navigate, location }) => (
+          <div className={styles.content}>
             <ul>
-              {
-                headings.map(item =>
-                  <li key={item.id} style={{marginLeft: `${(item.depth - 2) * 12}px`}}>
-                    <Link
-                      to={`${location.pathname}#${item.id}`}
-                      className={activeItemIds.includes(item.id) ? styles.active : ''}>{item.value}</Link>
-                  </li>
-                )
-              }
+              {headings.map(item => (
+                <li
+                  key={item.id}
+                  style={{ marginLeft: `${(item.depth - 2) * 12}px` }}
+                >
+                  <Link
+                    to={`${location.pathname}#${item.id}`}
+                    className={
+                      activeItemIds.includes(item.id) ? styles.active : ''
+                    }
+                  >
+                    {item.value}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
-        }
+        )}
       </Location>
     )
   }
 }
 
-export default Toc;
+export default Toc
