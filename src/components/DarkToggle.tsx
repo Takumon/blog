aa7dcb7beb11b8120195ddbd1957f0d000405d5c
Takumon <inouetakumon@gmail.com>
2020-08-04 08:06:14 +0900

    初期レンダリング時にスタイルが反映されないバグを修正

--- src/components/DarkToggle.tsx
+++ src/components/DarkToggle.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback } from 'react'
 import { css, keyframes } from '@emotion/core'
 import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -12,52 +12,45 @@ type ToggleProps = {
   stopMoving: () => void
 }
 
-const IconButton: React.FC<ToggleProps> = ({ icon, animation, toggle, moving, stopMoving }) => {
-  return (
-    <div css={[styles.content, moving && animation]} onClick={toggle} onAnimationEnd={stopMoving}>
-      <FontAwesomeIcon icon={icon} size={'2x'} />
-    </div>
-  )
-}
-
 export const DarkToggle: React.FC = () => {
-  const [moving, setMoving] = useState(true)
-  console.log('init!!')
+  const [moving, setMoving] = useState(false)
 
   const stopMoving = useCallback(() => {
-    console.log('stopMoving')
     setMoving(false)
   }, [])
 
   const startMoving = useCallback(() => {
-    console.log('startMoving')
     setMoving(true)
   }, [])
 
   return (
-    <ThemeToggler>
-      {({ theme, toggleTheme }) => {
-        if (!theme) {
-          return null
-        }
+    <div css={styles.content}>
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => {
+          if (!theme) {
+            return null
+          }
 
-        const isDark = theme === 'dark'
-        const toggle = () => {
-          toggleTheme(isDark ? 'light' : 'dark')
-          startMoving()
-        }
+          const isDark = theme === 'dark'
+          const toggle = () => {
+            toggleTheme(isDark ? 'light' : 'dark')
+            startMoving()
+          }
 
-        return (
-          <div css={styles.content} onClick={toggle} onAnimationEnd={stopMoving}>
-            {isDark ? (
-              <FontAwesomeIcon css={[styles.icon, moving && styles.spin]} icon={faMoon} />
-            ) : (
-              <FontAwesomeIcon css={[styles.icon, moving && styles.spinRevert]} icon={faSun} />
-            )}
-          </div>
-        )
-      }}
-    </ThemeToggler>
+          const icon = isDark ? (
+            <FontAwesomeIcon css={[styles.icon, moving && styles.spin]} icon={faMoon} />
+          ) : (
+            <FontAwesomeIcon css={[styles.icon, moving && styles.spinRevert]} icon={faSun} />
+          )
+
+          return (
+            <div onClick={toggle} onAnimationEnd={stopMoving}>
+              {icon}
+            </div>
+          )
+        }}
+      </ThemeToggler>
+    </div>
   )
 }
 
--- types/graphql-types.d.ts
+++ types/graphql-types.d.ts
@@ -18715,8 +18715,6 @@ export type QueryAllSitePageArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
@@ -18877,8 +18875,6 @@ export type QueryAllSitePluginArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
-  port?: Maybe<Scalars['Int']>;
-  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
@@ -19082,8 +19078,6 @@ export type SiteFieldsEnum =
   | 'siteMetadata___author'
   | 'siteMetadata___description'
   | 'siteMetadata___siteUrl'
-  | 'port'
-  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -19176,8 +19170,6 @@ export type SiteFieldsEnum =
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
